da4634c5d307503d32f4f77adfb3e474b7333ae0
Sina <sina@thewolpers.com>
2022-12-15 22:33:55 -0800

    Add script

--- script.js
+++ script.js
@@ -9,8 +9,24 @@ let cardDB = [
 	},
 ];
 
+let greeting = document.getElementById('greeting');
 let cardGrid = document.getElementById('cards');
+let cardTemplate = document.getElementById('project-card');
 
 function createCard() {
 	
 }
+
+function setGreeting() {
+	let objDate = new Date();
+	let time = objDate.getHours();
+	if (time < 12 && time > 4) {
+		greeting.textContent = 'Good morning,';
+	} else if (time >= 12 && time < 5) { 
+		greeting.textContent = 'Good afternoon,';
+	} else {
+		greeting.textContent = 'Good evening,';
+	}
+}
+
+setGreeting();
