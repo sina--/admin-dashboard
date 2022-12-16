ee6afef8d52227bd3ce9822945cb0ffd3a890ae3
Sina <sina@thewolpers.com>
2022-12-15 22:14:40 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let cardDB = [
+	{
+		'title': 'Super Cool Project',
+		'content': 'hello hello hello'
+	},
+	{
+		'title': 'Super Cool Project',
+		'content': 'hello hello hello'
+	},
+];
+
+let cardGrid = document.getElementById('cards');
+
+function createCard() {
+	
+}
